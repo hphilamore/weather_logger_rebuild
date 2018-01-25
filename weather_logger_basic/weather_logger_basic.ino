6f90c2e71bdb098c893273870968910ad01f6fca
hphilamore <hemmaphilamore@gmail.com>
2018-01-25 19:25:35 +0900

    time stamp logged to output file

--- weather_logger_basic/weather_logger_basic.ino
+++ weather_logger_basic/weather_logger_basic.ino
@@ -113,6 +113,7 @@ void loop() {
   digitalWrite(greenLED, LOW);          // LED off to show readings have been taken
      
   //column_headings_to_SD();
+  save_to_SD();
   
   delay(100);
 
@@ -152,6 +153,61 @@ void errorLED(uint8_t errno) {
     }
   }
 }
+
+
+void save_to_SD(){
+  /* 
+   *  Saves data to SD card (and optionally prints to serial)
+   *  Parameters not being loggedshould be commented out
+   */
+  // open the file to write data
+  logfile = SD.open(filename, FILE_WRITE);
+  
+  // if there is no SD card, print error message
+  if( ! logfile ) {
+    Serial.print("Couldnt create/open file: ");
+    Serial.println(filename);
+  }
+
+   
+  // write timestamp to file on SD card (and serial monitor)
+  logfile.print(rtc.getDay()); logfile.print("/"); logfile.print(rtc.getMonth()); logfile.print("/"); logfile.print(rtc.getYear());
+  logfile.print("\t");    
+  logfile.print(rtc.getHours()); logfile.print(":"); logfile.print(rtc.getMinutes()); logfile.print(":"); logfile.print(rtc.getSeconds());
+  logfile.print("\t");
+
+
+  if (ECHO_TO_SERIAL){
+    Serial.print(rtc.getDay()); Serial.print("/"); Serial.print(rtc.getMonth()); Serial.print("/"); Serial.print(rtc.getYear());
+    Serial.print("\t");
+    Serial.print(rtc.getHours()); Serial.print(":"); Serial.print(rtc.getMinutes()); Serial.print(":"); Serial.print(rtc.getSeconds());
+    Serial.print("\t");
+  }
+
+
+//  // write uv index to file on SD card (and serial monitor)
+//  logfile.print(UVindex); 
+//  logfile.print("\t");
+//  if (ECHO_TO_SERIAL){
+//    Serial.print(UVindex);
+//    Serial.print("\t");
+//  }
+
+
+//  // write windspeed to file on SD card (and serial monitor)
+//  logfile.print(windspeed); 
+//  logfile.print("\t");
+//  if (ECHO_TO_SERIAL){
+//    Serial.print(windspeed);
+//    Serial.print("\t");
+//  }
+  
+//  logfile.print("\n");
+//  if (ECHO_TO_SERIAL){  
+//    Serial.print("\n");
+//  }
+
+ }
 
 //void column_headings_to_SD(){
 // /*  
