78b64fb6d727b8e413b10971db6f5be3637d0d75
hphilamore <hemmaphilamore@gmail.com>
2018-01-30 09:48:47 +0900

    led flas and log instance seperate events. delay needed for LED to flash

--- weather_logger_basic/weather_logger_basic.ino
+++ weather_logger_basic/weather_logger_basic.ino
@@ -17,16 +17,12 @@ int D = 27, M = 1, Y = 18;
 int h = 15, m = 12, s = 0; 
 
 // User defined parameters
-#define LOG_INTERVAL 60000      // mills between entries
-#define LED_INTERVAL 1000       // mills between LED flashes to indicate logging is in process
+#define LOG_INTERVAL 30000       // mills between entries
+#define LED_INTERVAL 1000       // mills between entries
 #define ECHO_TO_SERIAL true     // echo data to serial port
 #define anemometer true         // measure anemometer
 #define wind_vane true          // measure wind_vane
-<<<<<<< HEAD
-#define UVsensor false          // measure UV index - PROGRAM WILL HANG IF UV SENSIR NOT PRESENT BUT VARIABLE SET TO TRUE             
-=======
-#define UVsensor true          // measure UV index
->>>>>>> parent of eeb048c... bug - program will hang if UV sensor not attached but UV sensing set to true
+#define UVsensor false          // measure UV index
 #define PV_cell true           // measure PV cell power output
 
 #define redLED 13
@@ -37,8 +33,8 @@ char filename[15];              // create text array with desired number of char
 File logfile;                   // create logging file object
 //bool first_loop = true;       // defines operations that only happen the first time the code loops
 
-unsigned long T_old;           // timer for logging
-unsigned long TLED_old;        // timer for LED flash to show logging
+unsigned long T_old;
+unsigned long TLED_old;
 
 void setup() {
   
@@ -88,7 +84,6 @@ void setup() {
   column_headings_to_SD();
 
   T_old = millis();
-  TLED_old = millis();
 
   if( anemometer ){
     T_old_anemom = millis();
@@ -97,11 +92,11 @@ void setup() {
 
 
   if( UVsensor ){
-    uv.begin();
-//    if (! uv.begin()) {
-//      Serial.println("Didn't find Si1145 UV meter");
-//      
-//    }  
+    //uv.begin();
+    if (! uv.begin()) {
+      Serial.println("Didn't find Si1145 UV meter");
+      
+    }  
   }
 
   
@@ -112,16 +107,16 @@ uint8_t i=0;
 
 void loop() {
 
-//  if ((millis() - TLED_old) > LED_INTERVAL){ 
-//    digitalWrite(greenLED, HIGH);         // LED on to show device is logging
-//    digitalWrite(greenLED, LOW);          
-//    TLED_old = millis();  
-//  }
-
+  if ((millis() - TLED_old) > LED_INTERVAL){
+    digitalWrite(greenLED, HIGH);         // LED on to show device is on
+    delay(20);
+    TLED_old = millis();
+    digitalWrite(greenLED, LOW);         // LED on to show device is on
+  }
 
   if ((millis() - T_old) > LOG_INTERVAL){ 
     
-    // digitalWrite(greenLED, HIGH);         // LED on to show device is on
+    digitalWrite(greenLED, HIGH);         // LED on to show device is on
 
     if( anemometer ){
       wind_speed();
@@ -142,7 +137,7 @@ void loop() {
     save_to_SD();
     T_old = millis();  
 
-    // digitalWrite(greenLED, LOW);          // LED off to show readings have been taken
+    digitalWrite(greenLED, LOW);          // LED off to show readings have been taken
     
   }
 } 
